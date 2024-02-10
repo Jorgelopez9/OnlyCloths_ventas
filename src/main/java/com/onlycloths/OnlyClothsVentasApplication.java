c9dce0a416549ae2cfbb1771a9a498cac40d55c8
jorge lopez <lopezcordovajorge3@gmail.com>
2024-02-10 13:57:51 -0500

    mi primera pagina

---
+++ src/main/java/com/onlycloths/OnlyClothsVentasApplication.java
@@ -0,0 +1,13 @@
+package com.onlycloths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyClothsVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyClothsVentasApplication.class, args);
+	}
+
+}
